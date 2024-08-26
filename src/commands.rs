6e7b9c32483770b0ee9588219218bac4f7a39938
Grégoire PELEGRIN <gregoire.pelegrin@intradef.gouv.fr>
2024-08-26 16:57:12 +0200

    Stubs

---
+++ src/commands.rs
@@ -0,0 +1,21 @@
+use std::path::PathBuf;
+
+pub enum PngMeArgs {
+    Encode(EncodeArgs),
+    Decode(DecodeArgs),
+    Remove(RemoveArgs),
+    Print(PrintArgs),
+}
+
+pub struct EncodeArgs {
+    // todo
+}
+pub struct DecodeArgs {
+    // todo
+}
+pub struct RemoveArgs {
+    // todo
+}
+pub struct PrintArgs {
+    // todo
+}
